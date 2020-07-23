3fa095d23f5d78382a919d759cc81ecc22c20e4b
hardy <hard.t.z.wei@oocl.com>
2020-07-23 12:56:20 +0800

    [hardy]refactor: refactor FizzBuzzWhizz class

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -5,24 +5,25 @@ public class FizzBuzzWhizz {
     private static String FIZZ = "Fizz";
     private static String BUZZ = "Buzz";
     private static String WHIZZ = "Whizz";
+    private static String EMPTY = "";
 
     public String playFizzBuzzWhizz(int input) {
-        String result = "";
+        String result = EMPTY;
         result += checkFizz(input);
         result += checkBuzz(input);
         result += checkWhizz(input);
-        return result.equals("") ? String.valueOf(input) : result;
+        return result.equals(EMPTY) ? String.valueOf(input) : result;
     }
 
     private String checkFizz(int input) {
-        return input % 3 == 0 ? FIZZ : "";
+        return input % 3 == 0 ? FIZZ : EMPTY;
     }
 
     private String checkBuzz(int input) {
-        return input % 5 == 0 ? BUZZ : "";
+        return input % 5 == 0 ? BUZZ : EMPTY;
     }
 
     private String checkWhizz(int input) {
-        return input % 7 == 0 ? WHIZZ : "";
+        return input % 7 == 0 ? WHIZZ : EMPTY;
     }
 }

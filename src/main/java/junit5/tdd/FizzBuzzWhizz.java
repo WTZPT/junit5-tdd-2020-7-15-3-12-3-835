4b0c0a9d3e8384ed8ac2adffb109d30a1ccf86e1
hardy <hard.t.z.wei@oocl.com>
2020-07-23 11:48:37 +0800

    [hardy]update: update  FizzBuzzWhizz class, complete should_return_Whizz_when_given_7 test

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -4,6 +4,7 @@ public class FizzBuzzWhizz {
 
     private static String FIZZ = "Fizz";
     private static String BUZZ = "Buzz";
+    private static String WHIZZ = "Whizz";
 
     public String playFizzBuzzWhizz(int input) {
         if(input % 3 == 0) {
@@ -11,6 +12,9 @@ public class FizzBuzzWhizz {
         }
         if(input % 5 == 0) {
             return BUZZ;
+        }
+        if(input % 7 == 0){
+            return WHIZZ;
         }
         return String.valueOf(input);
     }
